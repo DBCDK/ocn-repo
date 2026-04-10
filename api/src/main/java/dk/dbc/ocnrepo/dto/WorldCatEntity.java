840c2445e642881cccfc19d0396a8e7ae5ef6c47
Jan Bauer Nielsen <jbn@dbc.dk>
2026-04-10 10:14:58 +0200

    Tidy pom.xml's

--- api/src/main/java/dk/dbc/ocnrepo/dto/WorldCatEntity.java
+++ api/src/main/java/dk/dbc/ocnrepo/dto/WorldCatEntity.java
@@ -1,8 +1,3 @@
-/*
- * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU 3
- * See license text in LICENSE.txt
- */
-
 package dk.dbc.ocnrepo.dto;
 
 import dk.dbc.commons.jpa.converter.StringListToPgTextArrayConverter;
